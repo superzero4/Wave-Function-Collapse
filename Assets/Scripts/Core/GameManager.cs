a684563cb01511d1efc733a06a2897f9f2c808b3
superzero4 <78915984+superzero4@users.noreply.github.com>
2022-11-11 17:18:26 +0100

    Updated the runner, manager to work accordingly with what was modified
    Removed any dependency to UnityEngine in the WFC module

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,7 +1,10 @@
 ﻿using superzero4.WFC.Core.DataStructures;
+using superzero4.WFC.Core.DataStructures.Cells;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using superzero4.WFC.Core.DataStructures.Collection;
 
 namespace superzero4.WFC.Core
 {
@@ -11,21 +14,33 @@ namespace superzero4.WFC.Core
         [SerializeField] private int _maxX;
         [SerializeField] private int _maxY;
         [SerializeField] private List<GameObject> _gameObjects;
+        [SerializeField, Range(0, 3f)] private float _delay;
+
         public enum Types { one, two, three, four }
         void Start()
         {
             int sqr = (int)Mathf.Sqrt(_gameObjects.Count);
             Debug.Log("Using " + sqr + " sized matrix");
             var wfc
-                = new WFCRunner<GameObject,(int,int)>(new Matrix2D<GameObject>(sqr, sqr, _gameObjects));
-                /*= new WFCRunner<GameObject, Types>(new Collection1D<GameObject, Types>(new Dictionary<Types, GameObject>()
+                = new WFCRunner<CellBase<GameObject>, (int, int)>(new Matrix2D<CellBase<GameObject>>(sqr, sqr, _gameObjects.Select(g => new GameObjectCell(g))), new UnityDebugLogger());
+            //= new WFCRunner<GameObjectCell, int>(new List1D<GameObjectCell>(_gameObjects.Select(g=>new CellBase<GameObject>(g))), new UnityDebugLogger());
+            /*= new WFCRunner<GameObject, Types>(new Collection1D<GameObject, Types>(new Dictionary<Types, GameObject>()
                 {
                     { Types.one,_gameObjects[0] },
                     { Types.two,_gameObjects[1] },
                     { Types.three,_gameObjects[2] },
                     { Types.four,_gameObjects[3] },
                 }));*/
-            wfc.Run((1,1));
+            /*for (int i = 0; i < sqr; i++)
+                wfc.Run((i, i));*/
+            StartCoroutine(wfc.IterativeRun((1, 1), new WaitForSeconds(_delay)));
+        }
+        public class UnityDebugLogger : LoggerWrapper
+        {
+            public override void Log(string message)
+            {
+                Debug.Log(message);
+            }
         }
     }
 
--- Assets/Scripts/Core/WFCRunner.cs
+++ Assets/Scripts/Core/WFCRunner.cs
@@ -2,26 +2,116 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
+using superzero4.WFC.Core.DataStructures.Cells;
+using superzero4.WFC.Core.DataStructures.Collection;
 
 namespace superzero4.WFC.Core
 {
 
-    public class WFCRunner<Cell,Indexer> where Indexer : struct
+    public class WFCRunner<Cell, Indexer> where Indexer : struct where Cell : IMarkable, IActivable
     {
         private APositionEnumerable<Cell, Indexer> _enumerable;
+        private readonly LoggerWrapper Debug;
 
-        public WFCRunner(APositionEnumerable<Cell, Indexer> enumerable)
+        public WFCRunner(APositionEnumerable<Cell, Indexer> enumerable, LoggerWrapper logger)
         {
             _enumerable = enumerable;
+            Debug = logger;
         }
 
-        internal void Run(Indexer start=default)
+        public void Run(Indexer start = default)
         {
-            _enumerable.GetEnumerator();
-            foreach (Cell cell in _enumerable.Relatives(start))
-                Debug.Log(cell);
+            IEnumerable<Cell> dfs = Init(start);
+            foreach (Cell cell in dfs)
+            {
+                cell.Activate();
+            }
         }
+
+        public IEnumerator IterativeRun<Yield>(Indexer start = default, Yield yieldOnEachStep = null) where Yield : class
+        {
+            var dfs = Init(start);
+            foreach (Cell cell in dfs)
+            {
+                cell.Activate();
+                yield return yieldOnEachStep;
+            }
+        }
+        private IEnumerable<Cell> Init(Indexer start)
+        {
+            Debug.Log("Running from " + start);
+            _enumerable.GetEnumerator();
+            IEnumerable<Cell> dfs = new DFSRunner(_enumerable).DFS(start).Select(p => _enumerable[p]);
+            return dfs;
+        }
+
+        public class DFSRunner
+        {
+            private APositionEnumerable<Cell, Indexer> _enumerable;
+            private ControlStructure<Indexer> _controlStructure;
+            public DFSRunner(APositionEnumerable<Cell, Indexer> enumerable)
+            {
+                _enumerable = enumerable;
+                _controlStructure = new StackWrapper<Indexer>();
+            }
+            int loopCount = 0;
+            public IEnumerable<Indexer> DFS(Indexer start)
+            {
+                _controlStructure.Add(start);
+                foreach (var cell in _enumerable)
+                    cell.Marked = false;
+                while (_controlStructure.Count != 0 && loopCount < 100000)
+                {
+                    Indexer popped;
+                    loopCount++;
+                    popped = _controlStructure.Get();
+                    yield return popped;
+                    IEnumerable<Indexer> unMarkedRelatives = _enumerable.Enumerator.SafeRelativesPosition(popped).Where(rel => !_enumerable[rel].Marked);
+                    foreach (Indexer item in unMarkedRelatives)
+                    {
+                        OnAdded(popped);
+                        _controlStructure.Add(item);
+                    }
+                }
+            }
+
+            private void OnAdded(Indexer popped)
+            {
+                DoubleLog("Marked", popped);
+                _enumerable[popped].Marked = true;
+            }
+            public void DoubleLog(string msg, Indexer popped) => UnityEngine.Debug.Log(_enumerable[popped] + "  " + msg + " @ " + popped);
+        }
+    }
+    public abstract class ControlStructure<T>
+    {
+        public abstract void Add(T start);
+        internal abstract T Get();
+        public abstract int Count { get; }
+    }
+    public class StackWrapper<T> : ControlStructure<T>
+    {
+        private readonly Stack<T> _stack;
+
+        public StackWrapper()
+        {
+            _stack = new Stack<T>();
+        }
+
+        public override int Count => _stack.Count;
+        public override void Add(T start)
+        {
+            _stack.Push(start);
+        }
+        internal override T Get()
+        {
+            return _stack.Pop();
+        }
+    }
+    public abstract class LoggerWrapper
+    {
+        public abstract void Log(string message);
     }
 
 }
