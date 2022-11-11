33b03ccae9b385dcfd8fe82ba3f2fe3576350de2
superzero4 <78915984+superzero4@users.noreply.github.com>
2022-11-11 17:17:21 +0100

    Created classes and interface + one implementation for cell data types and their requirements

---
+++ Assets/Scripts/Core/DataStructures/Cells/CellBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace superzero4.WFC.Core.DataStructures.Cells
+{
+
+    public interface IMarkable
+    {
+        public bool Marked { get; set; }
+    }
+    public interface IActivable
+    {
+        public void Activate();
+    }
+    [System.Serializable]
+    public abstract class CellBase<T> : IMarkable,IActivable
+    {
+        [SerializeField]
+        protected T _data;
+
+        public CellBase(T data)
+        {
+            _data = data;
+        }
+
+        private bool _marked=false;
+        public bool Marked { get => _marked; set => _marked = value; }
+        public override string ToString()
+        {
+            return _data.ToString();
+        }
+
+        public abstract void Activate();
+    }
+}
---
+++ Assets/Scripts/Core/DataStructures/Cells/CellExamples.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace superzero4.WFC.Core.DataStructures.Cells
+{
+
+    public class GameObjectCell : CellBase<GameObject>
+    {
+        public GameObjectCell(GameObject data) : base(data) { }
+
+        public override void Activate()
+        {
+            _data.SetActive(true);
+            _data.GetComponent<Renderer>().material.SetColor(0, UnityEngine.Random.ColorHSV());
+        }
+    }
+
+}
